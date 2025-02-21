4cc23e380f6f9665ef06627088c2e2bba1d756fd
CHINEDU UTTAH <kelvinnino07@gmail.com>
2025-02-21 16:03:38 +0000

    Edit AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,10 +6,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/twrp_alioth.mk \
-	$(LOCAL_DIR)/twrp_munch.mk
+	$(LOCAL_DIR)/twrp_alioth.mk
 
 COMMON_LUNCH_CHOICES := \
-	twrp_alioth-eng \
-	twrp_munch-eng
+	twrp_alioth-eng
 #
